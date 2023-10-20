5d52b7a5f72bf4d3063d6ba9960f778bf6305d10
4runner99 <145167381+4runner99@users.noreply.github.com>
2023-10-20 14:16:46 -0700

    updated index.html and test.js

--- test.js
+++ test.js
@@ -1,10 +1,22 @@
-function random(number) {
-    return Math.floor(Math.random()*number);
+const rainbowColors = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#4B0082', '#9400D3'];
+let currentIndex = 0;
+let intervalId;
+const body = document.body;
+
+function startRainbowWave() {
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+    body.style.transition = 'background-color 0.5s ease'; // Optional: Add a smooth transition when stopping the animation
+  } else {
+    intervalId = setInterval(animateRainbow, 100); // Animate the rainbow every 100 milliseconds
+    body.style.transition = 'none'; // Remove transition for smoother color change
   }
+}
 
+function animateRainbow() {
+  body.style.backgroundColor = rainbowColors[currentIndex];
+  currentIndex = (currentIndex + 1) % rainbowColors.length;
+}
 
-  function bgChange() {
-    const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
-    document.body.style.backgroundColor = rndCol;
-  }
-
+document.getElementById('startButton').addEventListener('click', startRainbowWave);
